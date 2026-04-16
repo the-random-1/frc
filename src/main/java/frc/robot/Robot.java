d46eab1f59dca396b66932ee8fc03a96144b282e
Rishan Rajesh <rrajesh1742@Rishan-Rajesh-Grade10-620452.local>
2026-04-15 21:35:33 -0500

    first commit (holy FIRST reference)

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,62 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ * The methods in this class are called automatically corresponding to each mode, as described in
+ * the TimedRobot documentation. If you change the name of this class or the package after creating
+ * this project, you must also update the Main.java file in the project.
+ */
+public class Robot extends TimedRobot {
+    private static final String kDefaultAuto = "Default";
+    private static final String kCustomAuto = "My Auto";
+    private String m_autoSelected;
+    private final SendableChooser<String> m_chooser = new SendableChooser<>();
+
+    public Robot() {
+        // m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
+        // m_chooser.addOption("My Auto", kCustomAuto);
+        // SmartDashboard.putData("Auto choices", m_chooser);
+    }
+
+    @Override
+    public void robotPeriodic() {}
+
+    @Override
+    public void autonomousInit() {}
+
+    @Override
+    public void autonomousPeriodic() {}
+
+    @Override
+    public void teleopInit() {}
+
+    @Override
+    public void teleopPeriodic() {
+        System.out.println("rji4hutgy");
+    }
+
+    @Override
+    public void disabledInit() {}
+
+    @Override
+    public void disabledPeriodic() {}
+
+    @Override
+    public void testInit() {}
+
+    @Override
+    public void testPeriodic() {}
+
+    @Override
+    public void simulationInit() {}
+
+    @Override
+    public void simulationPeriodic() {}
+}
